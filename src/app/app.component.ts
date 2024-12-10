c64f5fec4c2097b0cd3802c8c5fb04dd3ac11e7e
jos3lo89 <gothangel21@hotmail.com>
2024-12-09 22:04:58 -0500

    logout btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { User } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -8,20 +10,49 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  isAuthenticated: boolean = false; 
+  /* =========== jso3lo ============ */
+  private _authService = inject(AuthService);
+  private _router = inject(Router);
 
-  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+  user: User | null = null;
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
+    this.getUser();
+  }
+
+  getUser() {
+    this._authService.authState$.subscribe({
+      next: (data) => {
+        console.log(data);
+        this.user = data;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  async logout() {
+    await this._authService.cerrarSesion();
+    this._router.navigateByUrl('/home');
+    this.getUser();
+  }
+
+  pushRoute(route: string) {
+    this._router.navigateByUrl(route);
+  }
+
+  /* =========== jso3lo ============ */
+
+  isAuthenticated: boolean = false;
 
   ngOnInit() {
-    
     this.checkAuthState();
   }
 
   private checkAuthState() {
-    this.afAuth.onAuthStateChanged(user => {
-      this.isAuthenticated = !!user; 
+    this.afAuth.onAuthStateChanged((user) => {
+      this.isAuthenticated = !!user;
 
-      
       if (!user) {
         this.router.navigate(['/home']);
       }
