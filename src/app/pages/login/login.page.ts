57a8cbfc877a2cbc808f1df73bef0f4fff79d570
jos3lo89 <gothangel21@hotmail.com>
2024-12-09 21:36:50 -0500

    register and login ok

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +13,56 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
+  /* =========== jso3lo ============ */
+
+  private _authService = inject(AuthService);
+  private _fb = inject(FormBuilder);
+  private _toastService = inject(ToastService);
+  private _router = inject(Router);
+
+  isloadingSubmitBtn = false;
+
+  form = this._fb.group({
+    email: this._fb.control('', [Validators.required, Validators.email]),
+    pwd: this._fb.control('', [Validators.required, Validators.min(6)]),
+  });
+
+  async registrar() {
+    console.log(this.form.value);
+
+    try {
+      this.isloadingSubmitBtn = true;
+
+      const { email, pwd } = this.form.value;
+      if (!email || !pwd) {
+        console.log('faltan datos');
+
+        return;
+      }
+
+      await this._authService.loginWithEmailAndPwd(email, pwd);
+
+      this.form.reset();
+
+      this.isloadingSubmitBtn = false;
+
+      this._router.navigateByUrl('/home');
+
+      await this._toastService.getToast('Vienvenido', 'middle', 'success');
+    } catch (error) {
+      console.log(error);
+
+      this.isloadingSubmitBtn = false;
+      await this._toastService.getToast(
+        'Fallo al iniciar sesión',
+        'middle',
+        'danger'
+      );
+    }
+  }
+
+  /* =========== jso3lo ============ */
+
   nombre: string = '';
   apellido: string = '';
   correo: string = '';
@@ -23,20 +76,17 @@ export class LoginPage {
   ) {}
 
   async register() {
-  
     if (!this.correo || !this.password || this.password.length < 6) {
       this.presentToast('Por favor, verifica los datos ingresados.');
       return;
     }
 
     try {
-      
       const userCredential = await this.afAuth.createUserWithEmailAndPassword(
         this.correo,
         this.password
       );
 
-      
       if (userCredential.user) {
         await this.afs.collection('users').doc(userCredential.user.uid).set({
           nombre: this.nombre,
@@ -46,12 +96,13 @@ export class LoginPage {
 
         this.presentToast('Usuario registrado exitosamente.');
 
-        
-        this.router.navigate(['/login']); 
+        this.router.navigate(['/login']);
       }
     } catch (error) {
       console.error('Error al registrar:', error);
-      this.presentToast('Error al registrar el usuario. Por favor, intenta nuevamente.');
+      this.presentToast(
+        'Error al registrar el usuario. Por favor, intenta nuevamente.'
+      );
     }
   }
 
--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ToastController } from '@ionic/angular';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-registro',
@@ -9,46 +12,96 @@ import { Router } from '@angular/router';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage {
+  /* =========== jso3lo ============ */
+
+  private _authService = inject(AuthService);
+  private _fb = inject(FormBuilder);
+  private _toastService = inject(ToastService);
+  isloadingSubmitBtn = false;
+
+  form = this._fb.group({
+    nombre: this._fb.control('', [Validators.required]),
+    apellido: this._fb.control('', [Validators.required]),
+    email: this._fb.control('', [Validators.required, Validators.email]),
+    pwd: this._fb.control('', [Validators.required, Validators.min(6)]),
+  });
+
+  async register() {
+    console.log(this.form.value);
+
+    try {
+      this.isloadingSubmitBtn = true;
+
+      const { apellido, email, nombre, pwd } = this.form.value;
+      if (!apellido || !email || !nombre || !pwd) {
+        console.log('faltan datos');
+
+        return;
+      }
+
+      await this._authService.registerWithEmailAndPwd({
+        apellido,
+        email,
+        nombre,
+        pwd,
+      });
+
+      await this._toastService.getToast(
+        'Registro exitoso',
+        'middle',
+        'success'
+      );
+
+      this.form.reset();
+
+      this.isloadingSubmitBtn = false;
+    } catch (error) {
+      console.log(error);
+
+      this.isloadingSubmitBtn = false;
+      await this._toastService.getToast(
+        'Fallo al registrarse',
+        'middle',
+        'danger'
+      );
+    }
+  }
+
+  /* =========== jso3lo ============ */
   correo: string = '';
   password: string = '';
 
-  constructor(
-    private afAuth: AngularFireAuth,
-    private toastController: ToastController,
-    private router: Router 
-  ) {}
+  constructor() {} // private router: Router // private toastController: ToastController, // private afAuth: AngularFireAuth,
 
-  async login() {
-    if (!this.correo || !this.password) {
-      this.presentToast('Por favor, ingrese su correo y contraseña.');
-      return;
-    }
+  // async login() {
+  //   if (!this.correo || !this.password) {
+  //     this.presentToast('Por favor, ingrese su correo y contraseña.');
+  //     return;
+  //   }
 
-    try {
-      const userCredential = await this.afAuth.signInWithEmailAndPassword(
-        this.correo,
-        this.password
-      );
+  //   try {
+  //     const userCredential = await this.afAuth.signInWithEmailAndPassword(
+  //       this.correo,
+  //       this.password
+  //     );
 
-      if (userCredential.user) {
-        this.presentToast('Inicio de sesión exitoso.');
+  //     if (userCredential.user) {
+  //       this.presentToast('Inicio de sesión exitoso.');
 
-        
-        this.router.navigate(['/home']); 
+  //       this.router.navigate(['/home']);
+  //     }
+  //   } catch (error) {
+  //     console.error('Error al iniciar sesión:', error);
+  //     this.presentToast('Correo o contraseña incorrectos.');
+  //   }
+  // }
 
-      }
-    } catch (error) {
-      console.error('Error al iniciar sesión:', error);
-      this.presentToast('Correo o contraseña incorrectos.');
-    }
-  }
-
-  async presentToast(message: string) {
-    const toast = await this.toastController.create({
-      message: message,
-      duration: 2000,
-      position: 'bottom',
-    });
-    toast.present();
-  }
+  // async presentToast(message: string) {
+  //   const toast = await this.toastController.create({
+  //     message: message,
+  //     duration: 2000,
+  //     position: 'bottom',
+  //   });
+  //   toast.present();
+  // }
 }
